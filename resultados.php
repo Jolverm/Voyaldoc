318818a93c1c437fa67b220411243ed0d801a041
AlexGaza <alejandro.garcia@sparklabs.com.mx>
2014-04-03 09:44:36 -0600

    Maquetacion de pantallas
    Maquetacion de pantallas
    buscador-filtro.php
    resultados.php
    perfil.php
    perfil-doctor.php

--- resultados.php
+++ resultados.php
@@ -1,8 +1,239 @@
-<?php include'header.php' ;?>
+per<?php include'header.php' ;?>
 
 <!-- Contenedor principal-->
 <div class="container"> 
   <?php include'buscador-filtro.php' ;?>
+  <!-- inicia contenido resultados de busqueda-->
+	<div class="col-lg-12 col-md-12 hidden-xs text_resultado">
+    Resultado de 
+    </div>
+    
+      <div class="col-lg-9 col-md-9 hidden-xs resultado"><!-- columna datos de consulta-->
+    	<div class="col-lg-2 col-md-2 hidden-xs avatar">
+        	<img src="img/avatar.png" class="img-responsive">
+        </div>
+
+        <div class="col-lg-3 col-md-3 hidden-xs">
+ 	        <div class="row titulo-dr">Dr. José Arturo Avilés Román</div>
+            <div class="row">
+            <span class="subtitulo">Alergólogo, Inmunólogo</span>
+            Consultorio Agrícola Oriental
+            Av Javier Rojo Goméz 228 SN
+            Col. Agrícola Oriental
+            Del. Iztacalco CP 08500
+            Distrito Federal 
+            </div>
+        </div>
+
+        <!--inicia el calendario de fechas -->
+            <div class="col-lg-1 col-md-1 semana-inicio">
+                <h4 class="tit-dia">Lun</h4>
+                31 Mar
+            </div> 
+            <div class="col-lg-1 col-md-1 semana1">
+                <h4 class="tit-dia">Mar</h4>
+                01 Abr
+            </div>
+            <div class="col-lg-1 col-md-1 semana">
+                <h4 class="tit-dia">Mié</h4>
+                02 Abr
+            </div>
+            <div class="col-lg-1 col-md-1 semana1">
+                <h4 class="tit-dia">Jue</h4>
+                03 Abr
+                <span class="horario"> 
+                16:00
+                16:30
+                17:00
+                17:30
+                18:00
+                18:30
+                </span>
+            </div>
+            <div class="col-lg-1 col-md-1 semana">
+                <h4 class="tit-dia">Vie</h4>
+                04 Abr
+                <span class="horario">             
+                16:00
+                16:30
+                17:00
+                17:30
+                18:00
+                18:30
+                </span>
+            </div>
+            <div class="col-lg-1 col-md-1 semana1">
+                <h4 class="tit-dia">Sáb</h4>
+                05 Abr
+                <span class="horario">             
+                16:00
+                16:30
+                17:00
+                17:30
+                18:00
+                18:30
+                </span>
+            </div>
+            <div class="col-lg-1 col-md-1 semana">
+                <h4 class="tit-dia">Dom</h4>
+                06 Abr
+            </div>
+        <!--termina el calendario de fechas -->    
+      </div><!-- termina columna datos de consulta-->    
+      
+      <div class="col-lg-3 col-md-3 hidden-xs aplicacion">
+      HOLA
+      </div>
+    
+      <div class="col-lg-9 col-md-9 hidden-xs resultado"><!-- columna datos de consulta-->
+    	<div class="col-lg-2 col-md-2 hidden-xs avatar">
+        	<img src="img/avatar.png" class="img-responsive">
+        </div>
+
+        <div class="col-lg-3 col-md-3 hidden-xs">
+ 	        <div class="row titulo-dr">Dr. José Arturo Avilés Román</div>
+            <div class="row">
+            <span class="subtitulo">Alergólogo, Inmunólogo</span>
+            Consultorio Agrícola Oriental
+            Av Javier Rojo Goméz 228 SN
+            Col. Agrícola Oriental
+            Del. Iztacalco CP 08500
+            Distrito Federal 
+            </div>
+        </div>
+
+        <!--inicia el calendario de fechas -->
+            <div class="col-lg-1 col-md-1 semana-inicio">
+                <h4 class="tit-dia">Lun</h4>
+                31 Mar
+            </div> 
+            <div class="col-lg-1 col-md-1 semana1">
+                <h4 class="tit-dia">Mar</h4>
+                01 Abr
+            </div>
+            <div class="col-lg-1 col-md-1 semana">
+                <h4 class="tit-dia">Mié</h4>
+                02 Abr
+            </div>
+            <div class="col-lg-1 col-md-1 semana1">
+                <h4 class="tit-dia">Jue</h4>
+                03 Abr
+                <span class="horario"> 
+                16:00
+                16:30
+                17:00
+                17:30
+                18:00
+                18:30
+                </span>
+            </div>
+            <div class="col-lg-1 col-md-1 semana">
+                <h4 class="tit-dia">Vie</h4>
+                04 Abr
+                <span class="horario">             
+                16:00
+                16:30
+                17:00
+                17:30
+                18:00
+                18:30
+                </span>
+            </div>
+            <div class="col-lg-1 col-md-1 semana1">
+                <h4 class="tit-dia">Sáb</h4>
+                05 Abr
+                <span class="horario">             
+                16:00
+                16:30
+                17:00
+                17:30
+                18:00
+                18:30
+                </span>
+            </div>
+            <div class="col-lg-1 col-md-1 semana">
+                <h4 class="tit-dia">Dom</h4>
+                06 Abr
+            </div>
+        <!--termina el calendario de fechas -->    
+      </div><!-- termina columna datos de consulta-->
+        
+      <div class="col-lg-9 col-md-9 hidden-xs resultado"><!-- columna datos de consulta-->
+    	<div class="col-lg-2 col-md-2 hidden-xs avatar">
+        	<img src="img/avatar.png" class="img-responsive">
+        </div>
+
+        <div class="col-lg-3 col-md-3 hidden-xs">
+ 	        <div class="row titulo-dr">Dr. José Arturo Avilés Román</div>
+            <div class="row">
+            <span class="subtitulo">Alergólogo, Inmunólogo</span>
+            Consultorio Agrícola Oriental
+            Av Javier Rojo Goméz 228 SN
+            Col. Agrícola Oriental
+            Del. Iztacalco CP 08500
+            Distrito Federal 
+            </div>
+        </div>
+
+        <!--inicia el calendario de fechas -->
+            <div class="col-lg-1 col-md-1 semana-inicio">
+                <h4 class="tit-dia">Lun</h4>
+                31 Mar
+            </div> 
+            <div class="col-lg-1 col-md-1 semana1">
+                <h4 class="tit-dia">Mar</h4>
+                01 Abr
+            </div>
+            <div class="col-lg-1 col-md-1 semana">
+                <h4 class="tit-dia">Mié</h4>
+                02 Abr
+            </div>
+            <div class="col-lg-1 col-md-1 semana1">
+                <h4 class="tit-dia">Jue</h4>
+                03 Abr
+                <span class="horario"> 
+                16:00
+                16:30
+                17:00
+                17:30
+                18:00
+                18:30
+                </span>
+            </div>
+            <div class="col-lg-1 col-md-1 semana">
+                <h4 class="tit-dia">Vie</h4>
+                04 Abr
+                <span class="horario">             
+                16:00
+                16:30
+                17:00
+                17:30
+                18:00
+                18:30
+                </span>
+            </div>
+            <div class="col-lg-1 col-md-1 semana1">
+                <h4 class="tit-dia">Sáb</h4>
+                05 Abr
+                <span class="horario">             
+                16:00
+                16:30
+                17:00
+                17:30
+                18:00
+                18:30
+                </span>
+            </div>
+            <div class="col-lg-1 col-md-1 semana">
+                <h4 class="tit-dia">Dom</h4>
+                06 Abr
+            </div>
+        <!--termina el calendario de fechas -->    
+      </div><!-- termina columna datos de consulta-->
+
+    
+
+<!-- termina contenido resultados de busqueda-->
 </div><!-- Contenedor principal-->
 
 <?php include'footer.php' ;?>
